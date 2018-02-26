f13379bcdcc2ace0c9ff06b3a0bcf99f1ee34a37
Divyansh Chowdhary <divyanshchowdhary96@gmail.com>
2018-02-26 12:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
+require('dotenv').config();
+
+var http = require('http');
+var Pool = require('pg').Pool;
 var express = require('express');
-var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(express.bodyParser());
+app.use(app.router);
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+var config = {
+	//host: 'db.imad.hasura-app.io',
+	//port: '5432',
+	//user: 'divyanshchowdhary2016',
+	//database: 'divyanshchowdhary2016',
+	//password: process.env.DATABASE_PASSWORD
+	
+}
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+var pool = new Pool(config); //connect to the database
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','login.html'));
+})
 
+app.post('/logStat',function(req,res){
+	var username = req.body.username;
+	var password = req.body.password;
+	res.send("FDD");
+})
 
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
+var port =8080;
+app.listen(8080,function () {
+	console.log('Listening on port');
+})
 
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
